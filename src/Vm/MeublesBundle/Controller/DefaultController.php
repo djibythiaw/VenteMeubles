f5deafac18f141647fe6b6ef2dd3f31595018e52
Djiby Thiaw <djibythiaw@gmail.com>
2014-08-05 23:46:10 +0000

    intégration de bootstrap

--- src/Vm/MeublesBundle/Controller/DefaultController.php
+++ src/Vm/MeublesBundle/Controller/DefaultController.php
@@ -6,8 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
+		$name = "Djiby";
         return $this->render('VmMeublesBundle:Default:index.html.twig', array('name' => $name));
     }
 }
