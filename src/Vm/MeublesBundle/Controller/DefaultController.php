7db86bdef585fef923d5a221622876eed5c80281
Djiby Thiaw <djibythiaw@gmail.com>
2014-10-22 23:52:05 +0000

    adaptation de la page des produits et details produit

--- src/Vm/MeublesBundle/Controller/DefaultController.php
+++ src/Vm/MeublesBundle/Controller/DefaultController.php
@@ -10,4 +10,14 @@ class DefaultController extends Controller
     {
         return $this->render('VmMeublesBundle:Vm:index.html.twig');
     }
+
+    public function shopAction()
+    {
+        return $this->render('VmMeublesBundle:Vm:shop.html.twig');
+    }
+
+    public function detailsAction()
+    {
+        return $this->render('VmMeublesBundle:Vm:product-details.html.twig');
+    }
 }
